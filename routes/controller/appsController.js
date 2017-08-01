20bf636ed2c0ddf1a12e0ac1317d01bd7663c456
zhujun1xiaoxin <1193030047@qq.com>
2017-08-01 09:22:55 +0800

    1.add files not in version
    Signed-off-by: zhujun1xiaoxin <1193030047@qq.com>

---
+++ routes/controller/appsController.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Administrator on 2017/7/31.
+ */
+var express = require('express');
+var router = express.Router();
+var Proxy = require('../service/proxy');
+
+router.get('/api/app', function(req, res){
+	var proxy = new Proxy(req, res);
+	proxy.forwardGetRequest();
+});
+
+router.delete('/api/app', function(req, res){
+	var proxy = new Proxy(req, res);
+	proxy.forwardDelRequest();
+});
+
+router.put('/api/app/', function(req, res){
+	var proxy = new Proxy(req, res);
+	proxy.forwardPutRequest();
+});
+
+router.post('/api/app/', function(req, res){
+	var proxy = new Proxy(req, res);
+	proxy.forwardPostRequest();
+})
+
+module.exports = router;
